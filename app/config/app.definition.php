81a22a34cf5ec97a13d2eb638e25af5a475c2f2b
minigg <6@minigg.cn>
2022-06-27 21:27:07 +0800

    Fix UA

--- app/config/app.definition.php
+++ app/config/app.definition.php
@@ -3,7 +3,7 @@
 $reqRet = file_get_contents(APP_DIR_CONFIG . "app.config.json");
 $resJson = json_decode($reqRet);
 
-define('DEFAULT_UA', 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0');
+define('DEFAULT_UA', "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0");
 define('CONFIG_VERSION', $resJson->version);
 define('CONFIG_ADMIN', $resJson->admin);
 define('CONFIG_ROBOT', $resJson->robot);
